1b867b344436a102a77b93fcd9e67f8e95b7842a
macbookpro <ChhhutraJr>
2019-08-16 00:04:29 +0700

    add Staff_information activity

--- app/src/main/java/com/example/fiplus/StaffInformation.java
+++ app/src/main/java/com/example/fiplus/StaffInformation.java
@@ -1,13 +1,46 @@
 package com.example.fiplus;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class StaffInformation extends AppCompatActivity {
+
+    TimePickerDialog picker;
+    EditText eText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_information);
+
+
+        eText = findViewById(R.id.input_date);
+        eText.setInputType(InputType.TYPE_NULL);
+
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minutes = cldr.get(Calendar.MINUTE);
+                // time picker dialog
+
+                picker = new TimePickerDialog(StaffInformation.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int sHour, int sMinute) {
+                        eText.setText(sHour + ":" + sMinute);
+                    }
+
+                },hour, minutes, true);
+                picker.show();
+            }
+        });
     }
 }
